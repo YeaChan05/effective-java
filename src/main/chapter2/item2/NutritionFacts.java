885f083ea1692835c984f60be277cb805296886b
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-07-28 23:30:18 +0900

    item 2. 생성자에 매개변수가 많으면 빌더를 고려하라(lombok 추가)

---
+++ src/main/chapter2/item2/NutritionFacts.java
@@ -0,0 +1,32 @@
+package chapter2.item2;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Singular;
+
+import java.util.List;
+
+/**
+ * package :  chapter2.item2
+ * fileName : NutritionFacts
+ * author :  ShinYeaChan
+ * date : 2023-07-28
+ */
+@Builder
+public class NutritionFacts {
+    @Builder.Default private int servingSize=10;
+    private int sodium;
+    private int carbohydrate;
+    private int servings;
+    @Singular
+    private List<String> names;
+    public static void main(String[] args) {
+        NutritionFacts nutritionFacts= NutritionFacts.builder()
+                .servings(10)
+                .carbohydrate(100)
+                .name("11")
+                .name("22")
+                .clearNames()
+                .build();
+    }
+}
